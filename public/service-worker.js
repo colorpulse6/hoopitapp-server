53902f66b17d01880b9f6f49ae5e28f8542acd52
Nic Barnes <colorpulse@gmail.com>
2020-07-03 11:55:47 +0200

    faf

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.675235d93b68462974a7425ff95ac85e.js"
+  "/precache-manifest.99f2a2b3c1d308848246ad85b846e5ba.js"
 );
 
 self.addEventListener('message', (event) => {
