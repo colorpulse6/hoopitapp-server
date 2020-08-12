9e6d5a11c7ba50414adc1cb68c650eb4f70ddb9c
Nic Barnes <colorpulse@gmail.com>
2020-08-12 23:19:48 +0200

    socket server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const http = require("http").Server(app);
+// const http = require("http").Server(app);
 let Message = require("./models/Message.Model");
 
 //ensure database is connected
@@ -18,7 +18,7 @@ const router = express.Router();
 //   })
   
 
-  const port = process.env.REACT_APP_SOCKET_URL || 5001;
+  const port = process.env.REACT_APP_SOCKET_URL || 5000;
   const io = require('socket.io')(port);
   io.on("connection", (socket) => {
     socket.on("room", function (room) {
@@ -46,9 +46,9 @@ const router = express.Router();
     });
   });
   
-  http.listen(port, () => {
-    console.log('listening on *:' + port);
-  });
+  // http.listen(port, () => {
+  //   console.log('listening on *:' + port);
+  // });
 
 
 //CORS
