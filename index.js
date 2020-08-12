9662ac74c3d5644be92ce434df69a7c16fde4885
Nic Barnes <colorpulse@gmail.com>
2020-08-12 22:14:42 +0200

    socket server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
+let Message = require("./models/Message.Model");
 
 //ensure database is connected
 var path = require("path");
@@ -10,7 +11,44 @@ const MongoStore = require("connect-mongo")(session);
 require("dotenv").config();
 const router = express.Router();
 
+// SOCKET.IO
+var socket = require('socket.io')
+const server = app.listen(4000, () => {
+    console.log('Server is running on ', 4000)
+  })
+  
 
+  // const port = process.env.REACT_APP_SOCKET_URL || 5001;
+  const io = socket(server);
+  io.on("connection", (socket) => {
+    socket.on("room", function (room) {
+      socket.join(room);
+    });
+  
+    // Listen to connected users for a new message.
+    socket.on("message", (msg) => {
+      // Create a message with the content and the name of the user.
+      const message = new Message({
+        content: msg.content,
+        name: msg.name,
+        team: msg.team,
+        imageUrl: msg.imageUrl,
+      });
+  
+      // Save the message to the database.
+      message.save((err) => {
+        if (err) return console.error(err);
+      });
+  
+      // Notify all other users about a new message.
+      socket.in(msg.team).emit("push", msg);
+      console.log(msg.team);
+    });
+  });
+  
+  // http.listen(port, () => {
+  //   console.log('listening on *:' + port);
+  // });
 
 
 //CORS
