a09b303779112156cbb101092bb5641d3c58037c
Nic Barnes <colorpulse@gmail.com>
2020-08-12 23:43:13 +0200

    socket server

---
+++ public/precache-manifest.f85b811e4c04f99c719b890b2cc24fa5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85ef29104809225204be8bfdc47b231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52dcfb47216f4c0fc877",
+    "url": "/static/css/2.f63d27b6.chunk.css"
+  },
+  {
+    "revision": "dba5d7f584fcf5b77a72",
+    "url": "/static/css/main.6f9457e9.chunk.css"
+  },
+  {
+    "revision": "52dcfb47216f4c0fc877",
+    "url": "/static/js/2.8d2e80f7.chunk.js"
+  },
+  {
+    "revision": "555c84de6ae509130f9023d853ebddcc",
+    "url": "/static/js/2.8d2e80f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba5d7f584fcf5b77a72",
+    "url": "/static/js/main.9a06dd46.chunk.js"
+  },
+  {
+    "revision": "308cdd577e304632fd8a",
+    "url": "/static/js/runtime-main.be70af35.js"
+  },
+  {
+    "revision": "15d3d63414b0891942ddfbc9407018a2",
+    "url": "/static/media/b-ball.15d3d634.png"
+  },
+  {
+    "revision": "5163d59fc7090534431e6105f121ad8e",
+    "url": "/static/media/profile-dummy.5163d59f.png"
+  }
+]);
